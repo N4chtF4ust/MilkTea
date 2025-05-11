984ce1164384714fe684f4c9ce7f35800c2e7b3b
N4chtF4ust <ronguinto57@gmail.com>
2025-05-11 13:02:18 -0100

    add some env

--- src/dbCon.java
+++ src/dbCon.java
@@ -1,11 +1,14 @@
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbCon {
-    private static final String URL = "jdbc:mysql://mainline.proxy.rlwy.net:34739/railway";
-    private static final String USER = "root";
-    private static final String PASSWORD = "ePZWkvXduIzqxqZoTjRcbJcJWEznWNWw";
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
